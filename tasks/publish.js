bfebbea944ca4734ac085d0d8bf1a03350452054
Sandro Simas <sandro.csimas@gmail.com>
2017-10-29 23:28:10 -0300

    Commiting package.json and tasks

---
+++ tasks/publish.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const projectPackage = require('../package');
+const fs = require('fs');
+const path = require('path');
+const childProcess = require('child_process');
+const GitHubApi = require('github');
+const Promise = require('bluebird');
+
+const REPOSITORY_NAME = 'chatz-javascript';
+const REPOSITORY_OWNER = 'chatz-io';
+const WORKING_DIR = path.resolve(__dirname, '../');
+const TEMP_DIR = '/tmp';
+const TEMP_REPOSITORY_DIR = `${TEMP_DIR}/${REPOSITORY_NAME}`;
+
+const execAsync = Promise.promisify(childProcess.exec);
+
+const gitHubApi = new GitHubApi();
+gitHubApi.authenticate({
+  type: 'token',
+  token: '49aa7a1e572c681a5f7773983359a57e71a15b30',
+});
+
+function exec(command, options) {
+  return execAsync(command, options || {cwd: WORKING_DIR});
+}
+
+function checkoutTag(version) {
+  return Promise.coroutine(function*() {
+    console.log(`Checking out the tag ${version}...`);
+    yield exec(`git checkout ${version}`);
+  })();
+}
+
+function buildProduction() {
+  return Promise.coroutine(function*() {
+    console.log('Building library...');
+    yield exec('npm run build-prod');
+    console.log('Building browser library...');
+    yield exec('npm run build-browser-prod');
+  })();
+}
+
+function prepareRepository() {
+  return Promise.coroutine(function*() {
+    console.log('Preparing Github repository...');
+    yield exec(`rm -rf ${TEMP_REPOSITORY_DIR}`);
+    yield exec(`git clone https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git ${REPOSITORY_NAME}`, {cwd: TEMP_DIR});
+    yield exec('rm -rf *', {cwd: TEMP_REPOSITORY_DIR});
+  })();
+}
+
+function copyFiles(version) {
+  return Promise.coroutine(function*() {
+    console.log('Copying files...');
+    yield exec(`cp dist/${projectPackage.name}.min.js ${TEMP_REPOSITORY_DIR}/${projectPackage.name}-${projectPackage.version}.min.js`)
+  })();
+}
+
+function pushFiles(version) {
+  return Promise.coroutine(function*() {
+    console.log('Committing, tagging and pushing files to Github repository...');
+    yield exec('git add .', {cwd: TEMP_REPOSITORY_DIR});
+    yield exec(`git commit -am 'Release ${version}'`, {cwd: TEMP_REPOSITORY_DIR});
+    yield exec(`git tag ${version}`, {cwd: TEMP_REPOSITORY_DIR});
+    yield exec('git push --tags', {cwd: TEMP_REPOSITORY_DIR});
+    yield exec('git push origin master', {cwd: TEMP_REPOSITORY_DIR});
+  })();
+}
+
+function createRelease(version) {
+  return Promise.coroutine(function*() {
+    console.log('Creating Github release...');
+    const createRelease = Promise.promisify(gitHubApi.repos.createRelease);
+    yield createRelease({
+      owner: REPOSITORY_OWNER,
+      repo: REPOSITORY_NAME,
+      tag_name: version,
+      name: `Release ${version}`,
+    });
+  })();
+}
+
+function publishToNpm() {
+  return Promise.coroutine(function*() {
+    console.log('Publishing to npm...');
+    yield exec('npm publish');
+  })();
+}
+
+// Run this if call directly from command line
+if (require.main === module) {
+  Promise.coroutine(function*() {
+    try {
+      const version = projectPackage.version;
+      console.log(`Publishing version ${version} to Github and npm...`);
+      yield checkoutTag(version);
+      yield buildProduction();
+      yield prepareRepository();
+      yield copyFiles(version);
+      yield pushFiles(version);
+      yield createRelease(version);
+      yield checkoutTag('master');
+      yield publishToNpm();
+      console.log(`Version ${version} published with success!`);
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  })();
+}
---
+++ tasks/release.js
@@ -0,0 +1,100 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const childProcess = require('child_process');
+const semver = require('semver');
+const Promise = require('bluebird');
+
+const readFileAsync = Promise.promisify(fs.readFile);
+const writeFileAsync = Promise.promisify(fs.writeFile);
+const execAsync = Promise.promisify(childProcess.exec);
+
+const WORKING_DIR = path.resolve(__dirname, '../');
+const GRADLE_FILE = path.join(WORKING_DIR, 'chatz/build.gradle');
+const VERSION_NAME_REGEX = /versionName '(\d+\.\d+\.\d+)'/;
+const VERSION_NAME_FORMAT = 'versionName \'%s\'';
+
+function exec(command, options) {
+  return execAsync(command, options || {cwd: WORKING_DIR});
+}
+
+function buildDevelopment() {
+  return Promise.coroutine(function*() {
+    console.log('Building project...');
+    yield exec('npm run build');
+  })();
+}
+
+function updateVersion(versionType) {
+  return Promise.coroutine(function*() {
+    console.log('Updating version...');
+    const projectGradle =  yield readFileAsync(GRADLE_FILE, 'utf8');
+    const match = VERSION_NAME_REGEX.exec(projectGradle);
+    if (!match) {
+      throw new Error('Could not get project version');
+    }
+    const version = match[1];
+    console.log(`  Current version is ${version}`);
+    const nextVersion = semver.inc(version, versionType);
+    console.log(`  Next version is ${nextVersion}`);
+    const updatedProjectGradle = projectGradle.replace(match[0], util.format(VERSION_NAME_FORMAT, nextVersion));
+    yield writeFileAsync(GRADLE_FILE, updatedProjectGradle);
+    return nextVersion;
+  })();
+}
+
+function commitFiles(version) {
+  return Promise.coroutine(function*() {
+    console.log('Committing files...');
+    yield exec(`git commit -am "Release ${version}"`);
+  })();
+}
+
+function createTag(version) {
+  return Promise.coroutine(function*() {
+    console.log(`Creating tag ${version}...`);
+    yield exec(`git tag ${version}`);
+  })();
+}
+
+function pushTag() {
+  return Promise.coroutine(function*() {
+    console.log('Pushing tag to remote...');
+    yield exec('git push --tags');
+  })();
+}
+
+function pushFiles() {
+  return Promise.coroutine(function*() {
+    console.log('Pushing commits to remote...');
+    yield exec('git checkout master && git pull origin master');
+    yield exec('git push origin master');
+  })();
+}
+
+// Run this if call directly from command line
+if (require.main === module) {
+  const versionType = process.argv[2];
+  if (!versionType || ['major', 'minor', 'patch'].indexOf(versionType) === -1) {
+    console.log('Usage:');
+    console.log('npm run release major|minor|patch');
+    process.exit(1);
+  }
+  Promise.coroutine(function*() {
+    try {
+      const version = yield updateVersion(versionType);
+      console.log(`Releasing version ${version} to remote...`);
+      yield buildDevelopment();
+      yield commitFiles(version);
+      yield createTag(version);
+      yield pushTag();
+      yield pushFiles();
+      console.log(`Version ${version} released with success!`);
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  })();
+}
