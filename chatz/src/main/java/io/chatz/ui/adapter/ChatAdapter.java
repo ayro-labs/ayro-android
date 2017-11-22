bab6169c1469489f62454700613ec826d39c2e8c
Sandro Simas <sandro.csimas@gmail.com>
2017-11-22 00:52:34 -0300

    Upgrading libraries;

--- chatz/src/main/java/io/chatz/ui/adapter/ChatAdapter.java
+++ chatz/src/main/java/io/chatz/ui/adapter/ChatAdapter.java
@@ -108,7 +108,7 @@ public class ChatAdapter extends BaseAdapter<ChatMessage, ChatAdapter.ChatMessag
 
   private Spanned fromHtml(String source) {
     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-      return Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY);
+      return Html.fromHtml(source, Html.FROM_HTML_MODE_COMPACT);
     } else {
       return Html.fromHtml(source);
     }
